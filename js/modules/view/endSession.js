8e7c682d1e7fea7001c1a44cefcc5e83bdb78275
srescio <simone.rescio@gmail.com>
2014-07-17 00:08:30 +0200

    completed log and power modals rendering, added styles for green button, added new images for 31x31 buttons that scale for smaller versions

--- js/modules/view/endSession.js
+++ js/modules/view/endSession.js
@@ -17,9 +17,49 @@ define(['backbone',
         },
         
         render: function() {
-            console.log(this.options);
+            this.logoff = {
+                title:'Disconnessione da Windows',
+                buttons:[
+                    {
+                        action:'switch',
+                        color:'btn-green',
+                        text:'Switch user',
+                        status:'es-disabled'
+                    },
+                    {
+                        action:'logoff',
+                        color:'btn-yellow',
+                        text:'Disconnect'
+                    }
+                ]
+            };
+            this.poweroff = {
+                title:'Spegni il computer',
+                buttons:[
+                    {
+                        action:'standby',
+                        color:'btn-yellow',
+                        text:'Stand by',
+                        status:'es-disabled'
+                    },
+                    {
+                        action:'off',
+                        color:'btn-red',
+                        text:'Spegni'
+                    },
+                    {
+                        action:'restart',
+                        color:'btn-green',
+                        text:'Riavvia'
+                    }
+                ]                
+            };
+            this.action = {};
             
-            this.$el.append(this.template);
+            (this.options.action==='logoff')?
+            this.action=this.logoff : this.action=this.poweroff;
+            
+            this.$el.append(this.template(this.action));
             this.$el.addClass('end-session-show');
             this.modal  = this.$el.find('.end-session');
             this.cancel = this.modal.find('.esb-cancel');
